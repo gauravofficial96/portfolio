bed3d6a4520229a0f8a0f7589bbe816a689603ee
gaurav kumar <gauravofficial96@gmail.com>
2018-06-20 01:09:23 +0530

    polish

--- jobs/views.py
+++ jobs/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from .models import Job
 def home(request):
     jobs=Job.objects
-    return render(request,'jobs/home.html',{'jobs':jobs} )
+    return render(request,'jobs\home.html',{'jobs':jobs} )
+
